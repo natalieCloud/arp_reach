c097a8cad8d525fc76618339dc4f5b1630e98643
Natalie Chmura <ntchmura@gmail.com>
2023-08-02 11:19:25 -0400

    Update: Submodule

---
+++ src/arp_resources/arp_reach/arp_reach/nodes/reach_client.cpp
@@ -0,0 +1,69 @@
+#include <rclcpp/rclcpp.hpp>
+#include "arp_msgs/srv/run_reach_study.hpp"
+
+#include <chrono>
+#include <memory>
+
+#include <string>
+#include <stdlib.h>
+#include <unistd.h>
+
+using namespace std::chrono_literals;
+
+//TODO Change:
+#include <filesystem>
+#include <stdio.h>
+
+namespace fs = std::filesystem;
+
+int main(int argc, char **argv) {
+
+    rclcpp::init(argc, argv);
+
+    std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("arp_reach_client");
+    rclcpp::Client<arp_msgs::srv::RunReachStudy>::SharedPtr client =
+        node->create_client<arp_msgs::srv::RunReachStudy>("run_reach_study");
+
+    auto request = std::make_shared<arp_msgs::srv::RunReachStudy::Request>();
+
+    //std::string yaml = "/arpaint_project/arp_reach_ws/arp_reach/src/arp_resources/arp_reach/arp_reach/config/study_config.yaml";
+    //auto cwd = getcwd();
+    //RCLCPP_INFO(rclcpp::get_logger("rclcpp"), cwd);
+    auto curr = fs::current_path();
+    //std::cout << curr.c_str() << std::endl;
+    const fs::path sub_yaml = "/arp_reach/src/arp_resources/arp_reach/arp_reach/config/study_config.yaml";
+    fs::path curr_yaml = curr / sub_yaml; 
+
+    std::cout << curr_yaml.c_str() << std::endl;
+
+    //std::string yamldir = getenv("HOME") + yaml;
+
+    request->yaml_filepath = curr_yaml.c_str();
+
+    request->config_name = "study_config";
+
+    std::string results = "arpaint_project/arp_reach_ws/install/reach_ros/share/reach_ros";
+    std::string resultsdir = getenv("HOME") + results;
+    request->results_dir = resultsdir;
+    
+    while(!client->wait_for_service(1s)) {
+        if(!rclcpp::ok()) {
+            RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting forservice. Exiting.");
+            return 0;
+        }
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Service not active, waiting for activity.....");
+    }
+
+    auto result = client->async_send_request(request);
+
+    if(rclcpp::spin_until_future_complete(node, result) == 
+        rclcpp::FutureReturnCode::SUCCESS)
+    {
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Sucessfully called arp_reach! :)");
+    } else {
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Failed to call arp_reach! :(");
+    }
+
+    rclcpp::shutdown();
+    return 0;
+}
--- src/arp_resources/arp_reach/arp_reach/nodes/reach_study_node.cpp
+++ src/arp_resources/arp_reach/arp_reach/nodes/reach_study_node.cpp
@@ -1,4 +1,4 @@
-#include <reach/reach_study.h>
+#include "reach/reach_study.h"
 
 #include "arp_msgs/srv/run_reach_study.hpp"
 #include "rclcpp/rclcpp.hpp"
@@ -14,11 +14,15 @@ void run_reach(const std::shared_ptr<arp_msgs::srv::RunReachStudy::Request> requ
 
     try
     {
-        const YAML::Node config = YAML::LoadFile(request->yaml_filepath);
-        const std::string config_name = request->config_name;
-        const boost::filesystem::path results_dir(request->results_dir);
-
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "testing try");
+        const YAML::Node config = YAML::LoadFile(request->yaml_filepath.c_str());
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "testing yaml");
+        const std::string config_name = request->config_name.c_str();
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "testing config name");
+        const boost::filesystem::path results_dir(request->results_dir.c_str());
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Service called, about to ruin reach node");
         reach::runReachStudy(config, config_name, results_dir, false);
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Reach node called! check the directory!");
 
         boost::filesystem::path subdir(config_name);
         boost::filesystem::path file("reach.db.xml");
